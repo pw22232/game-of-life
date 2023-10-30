a2d8fd10fc02ce56ac3111aef6e4a6dd6982662c
Thisisseanxu <seanxu10086@gmail.com>
2023-10-30 18:39:50 +0000

    Finish Step two, support multiple workers

--- gol-parallel/gol/distributor.go
+++ gol-parallel/gol/distributor.go
@@ -22,6 +22,13 @@ func build(height, width int) [][]uint8 {
 	return newMatrix
 }
 
+// makeImmutableWorld takes an existing gol world and make sure no one can change it.
+func makeImmutableWorld(world [][]uint8) func(y, x int) uint8 {
+	return func(y, x int) uint8 {
+		return world[y][x]
+	}
+}
+
 func calculateAliveCells(p Params, world [][]uint8) []util.Cell {
 	var aliveCells []util.Cell
 	for x := 0; x < p.ImageWidth; x++ {
@@ -34,37 +41,34 @@ func calculateAliveCells(p Params, world [][]uint8) []util.Cell {
 	return aliveCells
 }
 
-func calculateNextState(p Params, world [][]uint8) [][]uint8 {
-	var changeAliveCells []util.Cell
-	var changeDieCells []util.Cell
+func calculateNextState(startY, endY int, p Params, world func(y, x int) uint8) [][]uint8 {
+	worldNextState := build(endY-startY-1, p.ImageWidth)
+	for y := startY + 1; y < endY; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			worldNextState[y-startY-1][x] = world(y, x)
+		}
+	}
 	neighboursCount := 0
-	for yIndex := range world {
-		for xIndex := range world[yIndex] {
+	for yIndex := startY + 1; yIndex < endY; yIndex++ {
+		for xIndex := 0; xIndex < p.ImageWidth; xIndex++ {
 			neighboursCount = countLivingNeighbour(xIndex, yIndex, world, p)
-			if world[yIndex][xIndex] == 0 {
-				if neighboursCount == 3 { // 如果该节点当前为死亡状态，并且有三个邻居，则标记为复活
-					changeAliveCells = append(changeAliveCells, util.Cell{xIndex, yIndex})
+			if world(yIndex, xIndex) == 0 {
+				if neighboursCount == 3 { // 如果该节点当前为死亡状态，并且有三个邻居，则复活该节点
+					worldNextState[yIndex-startY-1][xIndex] = 255
 				}
 			} else {
-				if neighboursCount < 2 || neighboursCount > 3 { // 存活的节点邻居少于2个或多于3个则标记为濒死
-					changeDieCells = append(changeDieCells, util.Cell{xIndex, yIndex})
+				if neighboursCount < 2 || neighboursCount > 3 { // 存活的节点邻居少于2个或多于3个则杀死该节点
+					worldNextState[yIndex-startY-1][xIndex] = 0
 				}
 			}
 		}
 
 	}
-	// 结算复活和濒死的节点并在世界上做出改变
-	for _, cells := range changeAliveCells {
-		world[cells.Y][cells.X] = 255
-	}
-	for _, cells := range changeDieCells {
-		world[cells.Y][cells.X] = 0
-	}
-	return world
+	return worldNextState
 }
 
 // 判断一个节点有多少存活的邻居，会调用isAlive函数，返回存活邻居的数量
-func countLivingNeighbour(x int, y int, world [][]uint8, p Params) int {
+func countLivingNeighbour(x, y int, world func(y, x int) uint8, p Params) int {
 	liveNeighbour := 0
 	for line := y - 1; line < y+2; line += 2 { // 判断前一行和后一行
 		for column := x - 1; column < x+2; column++ { // 判断该行3个邻居是否存活
@@ -84,7 +88,7 @@ func countLivingNeighbour(x int, y int, world [][]uint8, p Params) int {
 }
 
 // 判断一个节点是否存活，支持超出边界的节点判断（上方超界则判断最后一行，左方超界则判断最后一列，以此类推）
-func isAlive(x int, y int, world [][]uint8, p Params) bool {
+func isAlive(x, y int, world func(y, x int) uint8, p Params) bool {
 	if x < 0 {
 		x = p.ImageWidth - 1
 	}
@@ -97,10 +101,14 @@ func isAlive(x int, y int, world [][]uint8, p Params) bool {
 	if y > p.ImageHeight-1 {
 		y = 0
 	}
-	if world[y][x] == 255 {
+	if world(y, x) == 255 {
 		return true
 	}
 	return false
+}
+
+func worker(startY, endY int, p Params, data func(y, x int) uint8, out chan<- [][]uint8) {
+	out <- calculateNextState(startY, endY, p, data)
 }
 
 // distributor divides the work between workers and interacts with other goroutines.
@@ -108,7 +116,6 @@ func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
 	world := build(p.ImageHeight, p.ImageWidth)
-	final := build(p.ImageHeight, p.ImageWidth)
 
 	turn := 0
 	c.ioCommand <- ioInput
@@ -121,20 +128,35 @@ func distributor(p Params, c distributorChannels) {
 		}
 	}
 
-	/*	f := calculateAliveCells(p, world)
-		for _, v := range f {
-			c.events <- CellFlipped{0, v}
+	for currentTurn := 0; currentTurn < p.Turns; currentTurn++ {
+		immutableWorld := makeImmutableWorld(world)
+		var outChannel []chan [][]uint8
+		averageHeight := p.ImageHeight / p.Threads
+		restHeight := p.ImageHeight % p.Threads
+		currentHeight := 0
+		size := averageHeight
+		for i := 0; i < p.Threads; i++ {
+			size = averageHeight
+			if i < restHeight {
+				size += 1
+			}
+			channel := make(chan [][]uint8)
+			outChannel = append(outChannel, channel)
+			go worker(currentHeight-1, currentHeight+size, p, immutableWorld, channel)
+			currentHeight += size
 		}
-	*/
-	if p.Threads == 1 {
-		for i := 0; i < p.Turns; i++ {
-			temp := build(p.ImageHeight, p.ImageWidth)
-			temp = calculateNextState(p, world)
-			world = temp
-			c.events <- TurnComplete{i + 1}
-			turn++
+
+		currentLineIndex := 0
+		var worldPart [][]uint8
+		var newWorld [][]uint8
+		for i := 0; i < p.Threads; i++ {
+			worldPart = <-outChannel[i]
+			for _, linePart := range worldPart {
+				newWorld = append(newWorld, linePart)
+			}
+			currentLineIndex += len(worldPart)
 		}
-		final = world
+		world = newWorld
 	}
 
 	// TODO: Execute all turns of the Game of Life.
@@ -146,7 +168,7 @@ func distributor(p Params, c distributorChannels) {
 	<-c.ioIdle
 
 	c.events <- StateChange{turn, Quitting}
-	c.events <- FinalTurnComplete{turn, calculateAliveCells(p, final)}
+	c.events <- FinalTurnComplete{turn, calculateAliveCells(p, world)}
 
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
