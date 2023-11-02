637770e81ccce2ed5870e1617e0fc135ac1b27fe
Thisisseanxu <seanxu10086@gmail.com>
2023-11-02 16:09:15 +0000

    first push

--- gol-parallel/gol/distributor.go
+++ gol-parallel/gol/distributor.go
@@ -2,6 +2,8 @@ package gol
 
 import (
 	"strconv"
+	"sync"
+	"time"
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
@@ -30,8 +32,8 @@ func makeImmutableWorld(world [][]uint8) func(y, x int) uint8 {
 	}
 }
 
-// calculateAliveCells 计算世界中有多少个存活的细胞
-func calculateAliveCells(p Params, world [][]uint8) []util.Cell {
+// findAliveCells 返回世界中所有存活的细胞
+func findAliveCells(p Params, world [][]uint8) []util.Cell {
 	var aliveCells []util.Cell
 	for x := 0; x < p.ImageWidth; x++ {
 		for y := 0; y < p.ImageHeight; y++ {
@@ -43,21 +45,33 @@ func calculateAliveCells(p Params, world [][]uint8) []util.Cell {
 	return aliveCells
 }
 
+func countAliveCells(p Params, immutableWord func(y, x int) uint8) int {
+	aliveCellsCount := 0
+	for x := 0; x < p.ImageWidth; x++ {
+		for y := 0; y < p.ImageHeight; y++ {
+			if immutableWord(y, x) == 255 {
+				aliveCellsCount++
+			}
+		}
+	}
+	return aliveCellsCount
+}
+
 // calculateNextState 会计算以startY列开始，endY-1列结束的世界的下一步的状态
-func calculateNextState(startY, endY, width, height int, world func(y, x int) uint8) [][]uint8 {
+func calculateNextState(startY, endY, width, height int, immutableWorld func(y, x int) uint8) [][]uint8 {
 	worldNextState := build(endY-startY, width)
 	// 将要处理的world部分的数据映射到worldNextState上
 	for y := startY; y < endY; y++ {
 		for x := 0; x < width; x++ {
-			worldNextState[y-startY][x] = world(y, x) // worldNextState的坐标系从y=0开始
+			worldNextState[y-startY][x] = immutableWorld(y, x) // worldNextState的坐标系从y=0开始
 		}
 	}
 	// 计算每个点周围的邻居并将状态写入worldNextState
 	neighboursCount := 0
 	for y := startY; y < endY; y++ {
 		for x := 0; x < width; x++ {
-			neighboursCount = countLivingNeighbour(x, y, width, height, world)
-			if world(y, x) == 0 && neighboursCount == 3 { // 死亡的细胞邻居刚好为3个时复活
+			neighboursCount = countLivingNeighbour(x, y, width, height, immutableWorld)
+			if immutableWorld(y, x) == 0 && neighboursCount == 3 { // 死亡的细胞邻居刚好为3个时复活
 				worldNextState[y-startY][x] = 255
 			} else if neighboursCount < 2 || neighboursCount > 3 { // 存活的细胞邻居少于2个或多于3个时死亡
 				worldNextState[y-startY][x] = 0
@@ -69,42 +83,52 @@ func calculateNextState(startY, endY, width, height int, world func(y, x int) ui
 }
 
 // countLivingNeighbour 通过调用 isAlive 函数判断一个节点有多少存活的邻居，返回存活邻居的数量
-func countLivingNeighbour(x, y, width, height int, world func(y, x int) uint8) int {
+func countLivingNeighbour(x, y, width, height int, immutableWorld func(y, x int) uint8) int {
 	liveNeighbour := 0
 	for line := y - 1; line < y+2; line += 2 { // 判断前一行和后一行
 		for column := x - 1; column < x+2; column++ { // 判断该行3个邻居是否存活
-			if isAlive(column, line, width, height, world) {
+			if isAlive(column, line, width, height, immutableWorld) {
 				liveNeighbour += 1
 			}
 		}
 	}
 	// 判断左右边的邻居是否存活
-	if isAlive(x-1, y, width, height, world) {
+	if isAlive(x-1, y, width, height, immutableWorld) {
 		liveNeighbour += 1
 	}
-	if isAlive(x+1, y, width, height, world) {
+	if isAlive(x+1, y, width, height, immutableWorld) {
 		liveNeighbour += 1
 	}
 	return liveNeighbour
 }
 
 // 判断一个节点是否存活，支持超出边界的节点判断（上方超界则判断最后一行，左方超界则判断最后一列，以此类推）
-func isAlive(x, y, width, height int, world func(y, x int) uint8) bool {
+func isAlive(x, y, width, height int, immutableWorld func(y, x int) uint8) bool {
 	x = (x + width) % width
 	y = (y + height) % height
-	if world(y, x) != 0 {
+	if immutableWorld(y, x) != 0 {
 		return true
 	}
 	return false
 }
 
 // 将任务分配到每个线程
-func worker(startY, endY int, p Params, data func(y, x int) uint8, out chan<- [][]uint8) {
-	out <- calculateNextState(startY, endY, p.ImageWidth, p.ImageHeight, data)
+func worker(startY, endY int, p Params, immutableWorld func(y, x int) uint8, out chan<- [][]uint8) {
+	out <- calculateNextState(startY, endY, p.ImageWidth, p.ImageHeight, immutableWorld)
+}
+
+func timer(ticker *time.Ticker, events chan<- Event, worldLock *sync.Mutex, turn *int, p Params, immutableWorld *func(y, x int) uint8) {
+	for {
+		<-ticker.C
+		worldLock.Lock()
+		events <- AliveCellsCount{CompletedTurns: *turn, CellsCount: countAliveCells(p, *immutableWorld)}
+		worldLock.Unlock()
+	}
 }
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
+	var worldLock sync.Mutex
 
 	// TODO: Create a 2D slice to store the world.
 	world := build(p.ImageHeight, p.ImageWidth)
@@ -121,10 +145,13 @@ func distributor(p Params, c distributorChannels) {
 		}
 	}
 
+	immutableWorld := makeImmutableWorld(world)
+	ticker := time.NewTicker(1 * time.Second)
+	go timer(ticker, c.events, &worldLock, &turn, p, &immutableWorld)
 	// 根据需要处理的回合数量进行循环
-	for currentTurn := 0; currentTurn < p.Turns; currentTurn++ {
+	for turn = 0; turn < p.Turns; {
 		// 将世界转换为函数来防止其被意外修改
-		immutableWorld := makeImmutableWorld(world)
+		immutableWorld = makeImmutableWorld(world)
 		var outChannel []chan [][]uint8
 		averageHeight := p.ImageHeight / p.Threads
 		restHeight := p.ImageHeight % p.Threads
@@ -132,6 +159,7 @@ func distributor(p Params, c distributorChannels) {
 		size := averageHeight
 		for i := 0; i < p.Threads; i++ {
 			size = averageHeight
+			// 将除不尽的部分分配到前几个threads中，每个threads一行
 			if i < restHeight {
 				size += 1
 			}
@@ -151,7 +179,11 @@ func distributor(p Params, c distributorChannels) {
 			}
 			currentLineIndex += len(worldPart)
 		}
+		worldLock.Lock()
 		world = newWorld
+		c.events <- TurnComplete{CompletedTurns: turn}
+		turn++
+		worldLock.Unlock()
 	}
 
 	// TODO: Execute all turns of the Game of Life.
@@ -162,8 +194,9 @@ func distributor(p Params, c distributorChannels) {
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
+	ticker.Stop()
 	c.events <- StateChange{turn, Quitting}
-	c.events <- FinalTurnComplete{turn, calculateAliveCells(p, world)}
+	c.events <- FinalTurnComplete{turn, findAliveCells(p, world)}
 
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
