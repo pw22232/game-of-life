d75d7ddbdb2c26b049582462adc3474c121a7d08
ThisIsSeanXu <tk22444@bristol.ac.uk>
2023-11-14 16:04:05 +0000

    change stubs

--- distributed/stubs/stubs.go
+++ distributed/stubs/stubs.go
@@ -1,8 +1,19 @@
 package stubs
 
+import "uk.ac.bris.cs/gameoflife/gol"
+
 type golBoard struct {
 	World  [][]uint8
 	Turn   int
 	Width  int
 	Height int
+}
+
+type nextStateRequest struct {
+	GolBoard golBoard
+	p        gol.Params
+}
+
+type nextStateResponse struct {
+	golBoard golBoard
 }
