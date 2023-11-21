3787bba1ea76e74405045d350433c9d87c2976eb
Thisisseanxu <seanxu10086@gmail.com>
2023-11-21 16:15:23 +0000

    Change server to use flippedCell method

--- distributed/server/server.go
+++ distributed/server/server.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"sync"
 	"uk.ac.bris.cs/gameoflife/stubs"
+	"uk.ac.bris.cs/gameoflife/util"
 )
 
 type Server struct {
@@ -45,38 +46,22 @@ func (s *Server) RunGol(req stubs.RunGolRequest, res *stubs.RunGolResponse) (err
 	s.worldHeight = req.GolBoard.Height
 	s.worldWidth = req.GolBoard.Width
 	s.processLock.Unlock()
-	averageHeight := req.GolBoard.Height / req.Threads
-	restHeight := req.GolBoard.Height % req.Threads
-	size := averageHeight
-
 	for turn = 0; turn < req.Turns; turn++ {
-		var outChannels []chan [][]uint8
-		currentHeight := 0
-		for i := 0; i < req.Threads; i++ {
-			size = averageHeight
-			// 将除不尽的部分分配到前几个threads中，每个threads一行
-			if i < restHeight {
-				size += 1
-			}
-			outChannel := make(chan [][]uint8)
-			outChannels = append(outChannels, outChannel)
-			go worker(currentHeight, currentHeight+size, req.GolBoard.Width, req.GolBoard.Height, world, outChannel)
-			currentHeight += size
-		}
-
-		var worldPart [][]uint8
-		var newWorld [][]uint8
-		for i := 0; i < req.Threads; i++ {
-			worldPart = <-outChannels[i]
-			for _, linePart := range worldPart {
-				newWorld = append(newWorld, linePart)
-			}
-		}
+		outChannel := make(chan []util.Cell)
+		go calculateNextState(0, req.GolBoard.Height, req.GolBoard.Width, req.GolBoard.Height, world, outChannel)
+		flippedCells := <-outChannel
 		s.processLock.Lock()
-		world = newWorld
-		s.world = newWorld
+		for _, flippedCell := range flippedCells {
+			if world[flippedCell.Y][flippedCell.X] == 255 {
+				world[flippedCell.Y][flippedCell.X] = 0
+			} else {
+				world[flippedCell.Y][flippedCell.X] = 255
+			}
+		}
+		s.world = world
 		s.currentTurn = turn + 1
 		s.processLock.Unlock()
+
 		select {
 		case <-s.quit:
 			return
@@ -152,37 +137,23 @@ func main() {
 	rpc.Accept(ln)
 }
 
-// build 接收长度和宽度并生成一个指定长度x宽度的2D矩阵
-func build(height, width int) [][]uint8 {
-	newMatrix := make([][]uint8, height)
-	for i := range newMatrix {
-		newMatrix[i] = make([]uint8, width)
-	}
-	return newMatrix
-}
-
 // calculateNextState 会计算以startY列开始，endY-1列结束的世界的下一步的状态
-func calculateNextState(startY, endY, width, height int, world [][]uint8) [][]uint8 {
-	worldNextState := build(endY-startY, width)
-	// 将要处理的world部分的数据映射到worldNextState上
-	for y := startY; y < endY; y++ {
-		for x := 0; x < width; x++ {
-			worldNextState[y-startY][x] = world[y][x] // worldNextState的坐标系从y=0开始
-		}
-	}
+func calculateNextState(startY, endY, width, height int, world [][]uint8, outChannel chan []util.Cell) {
+	// 计算所有需要改变的细胞
+	var flippedCells []util.Cell
 	// 计算每个点周围的邻居并将状态写入worldNextState
 	neighboursCount := 0
 	for y := startY; y < endY; y++ {
 		for x := 0; x < width; x++ {
 			neighboursCount = countLivingNeighbour(x, y, width, height, world)
 			if world[y][x] == 0 && neighboursCount == 3 { // 死亡的细胞邻居刚好为3个时复活
-				worldNextState[y-startY][x] = 255
+				flippedCells = append(flippedCells, util.Cell{X: x, Y: y})
 			} else if world[y][x] == 255 && (neighboursCount < 2 || neighboursCount > 3) { // 存活的细胞邻居少于2个或多于3个时死亡
-				worldNextState[y-startY][x] = 0
+				flippedCells = append(flippedCells, util.Cell{X: x, Y: y})
 			}
 		}
 	}
-	return worldNextState
+	outChannel <- flippedCells
 
 }
 
@@ -214,9 +185,4 @@ func isAlive(x, y, width, height int, world [][]uint8) bool {
 		return true
 	}
 	return false
-}
-
-// 将任务分配到每个线程
-func worker(startY, endY, width, height int, world [][]uint8, out chan<- [][]uint8) {
-	out <- calculateNextState(startY, endY, width, height, world)
 }
